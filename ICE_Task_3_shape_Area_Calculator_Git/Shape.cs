ff3f99d7ff236d685dc62d369975f6cfb9744f4a
schul <schul@DESKTOP-3NEDSJA>
2025-03-13 16:29:14 +0200

    Shape

---
+++ ICE_Task_3_shape_Area_Calculator_Git/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Task_3_shape_Area_Calculator_Git
+{
+    abstract class Shape
+    {
+       public virtual void Display()
+        {
+
+        }
+    }
+}
