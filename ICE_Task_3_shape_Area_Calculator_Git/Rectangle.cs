75b9f10d6fa9178b981a34d72588c06476b6a754
schul <schul@DESKTOP-3NEDSJA>
2025-03-13 16:30:16 +0200

    Rectangle

---
+++ ICE_Task_3_shape_Area_Calculator_Git/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Task_3_shape_Area_Calculator_Git
+{
+    class Rectangle : Shape, ICalculateArea
+    {
+        public double Length { get; set; }
+        public double breadth { get; set; }
+
+        public Rectangle(double length, double breadth)
+        {
+            this.Length = length;
+            this.breadth = breadth;
+        }
+
+        public double CalculateArea()
+        {
+            double area = Length * breadth;
+            return area;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Area of Rectangle: {CalculateArea()}");
+        }
+
+    }
+}
