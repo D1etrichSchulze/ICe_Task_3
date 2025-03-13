f7aea4c253a59f31ba44ad6364e0c0897585e235
schul <schul@DESKTOP-3NEDSJA>
2025-03-13 16:20:53 +0200

    Add project files.

---
+++ ICE_Task_3_shape_Area_Calculator_Git/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ICE_Task_3_shape_Area_Calculator_Git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
