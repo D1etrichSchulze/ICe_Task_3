282bfaf62900fe35a6a810c253808acbb38a6439
schul <schul@DESKTOP-3NEDSJA>
2025-03-13 16:26:27 +0200

    ICalculateArea

---
+++ ICE_Task_3_shape_Area_Calculator_Git/ICalculateArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Task_3_shape_Area_Calculator_Git
+{
+    interface ICalculateArea
+    {
+        double CalculateArea();
+    }
+}
