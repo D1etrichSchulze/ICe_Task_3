96e6529aee8cc6205f6367a2cadfb28331cac4c1
schul <schul@DESKTOP-3NEDSJA>
2025-03-13 16:31:32 +0200

    Circle

---
+++ ICE_Task_3_shape_Area_Calculator_Git/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Task_3_shape_Area_Calculator_Git
+{
+    class Circle : Shape, ICalculateArea
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            double area = Math.PI * Radius * Radius;
+            return area;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Area of Circle: {CalculateArea()}");
+        }
+    }
+}
